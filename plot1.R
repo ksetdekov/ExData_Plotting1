f7d248abecb359f94810264435bedff9ae9c10e3
Kirill Setdekov <ksetdekov@gmail.com>
2018-12-26 10:51:40 +0300

    finished plot1

--- plot1.R
+++ plot1.R
@@ -1,29 +1,55 @@
-if(!file.exists("./data/power.zip"))
+#download if missing
+if (!file.exists("./data/household_power_consumption.txt"))
 {
         dir.create("./data")
-        fileUrl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-        download.file(fileUrl,destfile = "./data/power.zip")   
-        dateDownloaded<-date()  
+        fileUrl <-
+                "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(fileUrl, destfile = "./data/power.zip")
+        dateDownloaded <- date()
+        unzip("./data/power.zip", exdir = "./data")
 }
 
 
-unzip("./data/power.zip",exdir="./data")
-
+#read
 library(readr)
-power <- read_delim("data/household_power_consumption.txt", 
-                    ";", escape_double = FALSE, col_types = cols(Time = col_time(format = "%H:%M:%S")), 
-                    na = "?", trim_ws = TRUE)
+power <- read_delim(
+        "data/household_power_consumption.txt",
+        ";",
+        escape_double = FALSE,
+        col_types = cols(Time = col_time(format = "%H:%M:%S")),
+        na = "?",
+        trim_ws = TRUE
+)
+# format and select days
 head(power)
-
+power$Date <- as.Date(power$Date, "%d/%m/%Y")
 power2 <- na.omit(power)
-power2$Timen <- as.numeric(power2$Time)
-#power2 <- power2[1:100000,]
-library(party)
- cfit3<-ctree(Voltage~Global_intensity,data=power2)
-plot(cfit3)
+head(power2)
+power3 <-
+        power2[power2$Date == "2007-02-01" | power2$Date == "2007-02-02", ]
+head(power3)
 
-power2$prederuptions <- predict(cfit3, power2)
-library(InformationValue)
-library(MLmetrics)
-#plotROC(faithful$eruptions, faithful$prederuptions)
-Gini(power2$prederuptions , power2$Voltage)
+#perform task
+png(
+        file = "plot1.png",
+        width = 480,
+        height = 480,
+        bg = "white"
+)
+hist(
+        power3$Global_active_power,
+        col = "red",
+        main = "Global Active Power",
+        xlab = "Global Active Power (kilowatts)"
+)
+dev.off()
+
+#plot the same to view
+hist(
+        power3$Global_active_power,
+        col = "red",
+        main = "Global Active Power",
+        xlab = "Global Active Power (kilowatts)"
+)
+rm(power2)
+gc()
